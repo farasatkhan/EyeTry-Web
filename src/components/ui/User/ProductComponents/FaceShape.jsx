0c190354bd94e8daad44bb28c3cf54f6711a626f
Farasat Khan <farasatkahan@gmail.com>
2023-11-30 15:09:59 +0500

    added button to filters for images

--- src/components/ui/User/ProductComponents/FaceShape.jsx
+++ src/components/ui/User/ProductComponents/FaceShape.jsx
@@ -1,62 +1,74 @@
-
 import React from "react";
 import FaceDetection from "../../../../pages/User/FaceDetection/FaceDetection";
+import FaceDetectionImages from "../../../../pages/User/FaceDetectionImages/FaceDetectionImages";
 
 const shapeFilter = ({ selectedShape, onShapeSelect }) => {
-    const sizeOptions = [
-        "All Shapes",
-        "Round Face",
-        "Square Face",
-        "Oval Face",
-        "Heart-shaped Face",
-        "Diamond Face",
-        "Rectangle/Long Face"
-    ];
+  const sizeOptions = [
+    "All",
+    "Round",
+    "Square",
+    "Oval",
+    "Heart-Shaped",
+    "Diamond",
+    "Rectangle/Long",
+  ];
 
-    const handleShapeClick = (shape) => {
-        // Update the selected shape when a shape is clicked
-        onShapeSelect(shape);
-        console.log("selected face shape in shapeFilter page: " + shape)
-    };
+  const handleShapeClick = (shape) => {
+    // Update the selected shape when a shape is clicked
+    onShapeSelect(shape);
+    console.log("selected face shape in shapeFilter page: " + shape);
+  };
 
-    return (
-        <>
-            <div className="absolute left-0 p-2 bg-white rounded-md shadow-lg z-50 w-[300px] cursor-pointer">
-                <ul className="space-y-2 cursor-pointer">
-                    {sizeOptions.map((shape, index) => (
-                        <li
-                            key={index}
-                            className={` rounded-sm py-1 px-1 transition duration-100 ease-in-out
-                            ${selectedShape === shape ? 'bg-gray-700 hover:bg-gray-700' : 'hover:bg-gray-100'}`}
-                        >
-                            <div onClick={() => handleShapeClick(shape)} className="cursor-pointer flex items-center">
-                                <input className="cursor-pointer w-5 h-4 mr-5"
-                                    type="checkbox"
-                                    id={shape}
-                                    checked={selectedShape === shape}
-                                    onChange={() => handleShapeClick(shape)}
-                                />
-                                {/* <img
+  return (
+    <>
+      <div className="absolute left-0 p-2 bg-white rounded-md shadow-lg z-50 w-[300px] cursor-pointer">
+        <ul className="space-y-2 cursor-pointer">
+          {sizeOptions.map((shape, index) => (
+            <li
+              key={index}
+              className={` rounded-sm py-1 px-1 transition duration-100 ease-in-out
+                            ${
+                              selectedShape === shape
+                                ? "bg-gray-700 hover:bg-gray-700"
+                                : "hover:bg-gray-100"
+                            }`}
+            >
+              <div
+                onClick={() => handleShapeClick(shape)}
+                className="cursor-pointer flex items-center"
+              >
+                <input
+                  className="cursor-pointer w-5 h-4 mr-5"
+                  type="checkbox"
+                  id={shape}
+                  checked={selectedShape === shape}
+                  onChange={() => handleShapeClick(shape)}
+                />
+                {/* <img
                                     src={shapeImages[shape]}
                                     className="w-6 h-3 mr-2"
                                 /> */}
-                                <label
-                                    htmlFor={shape}
-                                    className={`font-sans cursor-pointer ${selectedShape === shape ? "text-white " : ""
-                                        }`}
-                                >
-                                    {shape}
-                                </label>
-                            </div>
-                        </li>
-                    ))}
-                    <div className="rounded-sm py-2 px-4 transition duration-100 ease-in-out text-blue-400 bg-gray-200 hover:bg-gray-100 flex justify-center">
-                    <FaceDetection onFaceShapeChange={handleShapeClick} />
-                    </div>
-                </ul>
-            </div>
-        </>
-    );
+                <label
+                  htmlFor={shape}
+                  className={`font-sans cursor-pointer ${
+                    selectedShape === shape ? "text-white " : ""
+                  }`}
+                >
+                  {shape}
+                </label>
+              </div>
+            </li>
+          ))}
+          <div className="rounded-sm py-2 px-4 transition duration-100 ease-in-out text-blue-400 bg-gray-200 hover:bg-gray-100 flex justify-center">
+            <FaceDetection onFaceShapeChange={handleShapeClick} />
+          </div>
+          <div className="rounded-sm py-2 px-4 transition duration-100 ease-in-out text-blue-400 bg-gray-200 hover:bg-gray-100 flex justify-center">
+            <FaceDetectionImages />
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default shapeFilter;
---
+++ src/pages/User/FaceDetectionImages/FaceDetectionImages.jsx
@@ -0,0 +1,13 @@
+import React, { useRef, useState, useEffect } from "react";
+
+function FaceDetectionImages() {
+  return (
+    <div className="flex">
+      <div>
+        <button>Find Your Face Shape</button>
+      </div>
+    </div>
+  );
+}
+
+export default FaceDetectionImages;

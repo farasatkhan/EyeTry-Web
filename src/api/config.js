a8572bfaecda97c4cf3e1cbea37e2b35d4b00ca8
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-30 15:52:05 +0500

    axios config

---
+++ src/api/config.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import API_URL from "../config/config";
+
+
+import { getDataFromLocalStorage } from "../utils/LocalStorage";
+import { isAccessTokenExpired, refreshAccessToken } from "../utils/authUtils";
+
+const unauthenticatedAxiosInstance = axios.create({
+    baseURL: API_URL,
+});
+
+const authenticatedAxiosInstance = axios.create({
+    baseURL: API_URL,
+});
+
+authenticatedAxiosInstance.interceptors.request.use(
+    async (config) => {
+        const accessToken = getDataFromLocalStorage("accessToken");
+
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+        if (isAccessTokenExpired(accessToken)) {
+            const refreshToken = getDataFromLocalStorage("refreshToken");
+            const newAccessToken = await refreshAccessToken(refreshToken);
+
+            if (newAccessToken !== null) {
+                config.headers["Authorization"] = `Bearer ${newAccessToken}`;
+            } else {
+                window.location.href = '/signin';
+                return Promise.reject("The new access token is invalid.");
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export { authenticatedAxiosInstance, unauthenticatedAxiosInstance };

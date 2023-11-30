4b26a36ed80f38ea5ac20167965d32f7c8ac7033
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-30 15:49:14 +0500

    fixed jwt import

--- src/utils/privateRoutes/PrivateRoute.jsx
+++ src/utils/privateRoutes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
 import { reGenerateAccessToken, logoutUser} from '../../api/authapi';
 
 function PrivateRoute(props) {
